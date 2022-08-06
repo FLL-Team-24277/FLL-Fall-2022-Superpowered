bb13fa1a7387ed92c33da4653c232cb2c71827d8
FLL-Team-24277 <110741729+FLL-Team-24277@users.noreply.github.com>
2022-08-06 17:22:57 -0400

    Create operator.py

---
+++ spike/operator.py
@@ -0,0 +1,210 @@
+"""
+Please don't edit this file unless you know what you are doing. Making changes \
+to this file almost certaily will not do what you think it does.
+
+This file only helps with code completion and intellisense. It does not run \
+anything.
+"""
+def greater_than(a, b):
+    """
+    Tests whether value a is greater than value b.
+    
+    This is the same as a > b.
+    
+    Parameters
+    ---------
+    a : Any object that can be compared to b.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+
+    -----------------
+
+    b : Any object that can be compared to a.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+    
+    Returns
+    ---------
+    Type : boolean
+    
+    Values : True if a > b, otherwise False.
+    """
+    pass
+
+def greater_than_or_equal_to(a, b):
+    """
+    Tests whether value a is greater than or equal to value b.
+    
+    This is the same as a >= b.
+    
+    Parameters
+    ---------
+    a : Any object that can be compared to b.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+
+    -----------------
+
+    b : Any object that can be compared to a.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+    
+    Returns
+    ---------
+    Type : boolean
+    
+    Values : True if a >= b, otherwise False.
+    """
+    pass
+
+def less_than_or_equal_to(a, b):
+    """
+    Tests whether value a is less than or equal to value b.
+    
+    This is the same as a <= b.
+    
+    Parameters
+    ---------
+    a : Any object that can be compared to b.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+
+    -----------------
+
+    b : Any object that can be compared to a.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+    
+    Returns
+    ---------
+    Type : boolean
+    
+    Values : True if a <= b, otherwise False.
+    """
+    pass
+
+def less_than(a, b):
+    """
+    Tests whether value a is less than value b.
+    
+    This is the same as a < b.
+    
+    Parameters
+    ---------
+    a : Any object that can be compared to b.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+
+    -----------------
+
+    b : Any object that can be compared to a.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+    
+    Returns
+    ---------
+    Type : boolean
+    
+    Values : True if a < b, otherwise False.
+    """
+    pass
+
+def equal_to(a, b):
+    """
+    Tests whether value a is equal to value b.
+    
+    This is the same as a == b.
+    
+    Parameters
+    ---------
+    a : Any object that can be compared to b.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+
+    -----------------
+
+    b : Any object that can be compared to a.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+    
+    Returns
+    ---------
+    Type : boolean
+    
+    Values : True if a == b, otherwise False.
+    """
+    pass
+
+def not_equal_to(a, b):
+    """
+    Tests whether value a is not equal to value b.
+    
+    This is the same as a != b.
+    
+    Parameters
+    ---------
+    a : Any object that can be compared to b.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+
+    -----------------
+
+    b : Any object that can be compared to a.
+    
+    Type : any type
+    
+    Values : any value
+    
+    Default : no default value
+    
+    Returns
+    ---------
+    Type : boolean
+    
+    Values : True if a != b, otherwise False.
+    """
+    pass
