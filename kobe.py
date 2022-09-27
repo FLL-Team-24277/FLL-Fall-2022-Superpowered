27ced0e6f3d46037636ae7a79f69af10a00001d5
slcyed <jonaselsass@yahoo.com>
2022-09-27 15:44:25 -0500

    Edited Progarm

--- kobe.py
+++ kobe.py
@@ -1,4 +1,4 @@
-# LEGO type:standard slot:2 autostart
+# LEGO type:standard slot:0 autostart
 
 
 import base_robot
@@ -9,9 +9,17 @@ from spike.operator import greater_than, greater_than_or_equal_to, \
 
 br = base_robot.BaseRobot()
 
-# br.AccelGyroDriveForward(70)
-# br.GyroTurn(32)
-# br.AccelGyroDriveForward(16)
-br.rightMedMotor.run_for_degrees(130, -25)
-br.rightMedMotor.run_for_degrees(130,25)
-br.GyroTurn(90)
+br.AccelGyroDriveForward(60)
+br.GyroTurn(32)
+br.hub.motion_sensor.reset_yaw_angle()
+br.rightMedMotor.run_for_rotations(-0.1, -25)
+br.driveMotors.move_tank(16)
+br.rightMedMotor.run_for_rotations(0.1, -25)
+br.rightMedMotor.run_for_rotations(-0.1, -25)
+br.GyroTurn(-30)
+br.driveMotors.move_tank(7)
+br.GyroTurn(42)
+br.AccelGyroDriveForward(16)
+br.GyroTurn(42)
+br.AccelGyroDriveForward(30)
+br.rightMedMotor.run_for_rotations(0.3, -25)
