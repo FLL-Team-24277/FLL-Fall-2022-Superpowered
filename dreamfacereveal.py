a21ca25a6dee1778744c868f299492af002af38c
slcyed <91488376+slcyed@users.noreply.github.com>
2022-10-29 09:40:06 -0500

    Update dreamfacereveal.py

--- dreamfacereveal.py
+++ dreamfacereveal.py
@@ -1,4 +1,6 @@
 # LEGO type:standard slot:1 autostart
+
+
 import base_robot
 import sys
 from spike import MotorPair
@@ -7,39 +9,52 @@ from spike.operator import greater_than, greater_than_or_equal_to, \
     less_than, less_than_or_equal_to, equal_to, not_equal_to
 
 br = base_robot.BaseRobot()
-#Function for Resetting Yaw Angle
+#Function for Resetting Yaw Angle and debugging
 Debugging = True
 def debug(text):
     if Debugging:
         print(text)
 def ResetGyro():
     br.hub.motion_sensor.reset_yaw_angle()
-#Set Attachment Motors
-attachment = MotorPair(br._leftAttachmentMotorPort,br._rightAttachmentMotorPort)
-debug("Drive forward on left")
-br.AccelGyroDriveForward(70)
+
+br.AccelGyroDriveForward(90)
+# #Set Attachment Motors
+# attachment = MotorPair(br._leftAttachmentMotorPort,br._rightAttachmentMotorPort)
+# debug("Drive forward on left")
+# br.AccelGyroDriveForward(70)
+# ResetGyro()
+# debug("Turn Right")
+# br.GyroTurn(90)
+# debug("Move a small amount")
+# br.driveMotors.move_tank(18, 'cm', 50, 50)
+# ResetGyro()
+# debug("Turn Back")
+# br.GyroTurn(-83)
+# ResetGyro()
+# debug("Rotate Attachment")
+# attachment.move_tank(.9, 'rotations' ,-25, -25)
+# debug("Pick up Next Unit")
+# br.AccelGyroDriveForward(20)
+# ResetGyro()
+# br.GyroTurn(59)
+# br.driveMotors.move_tank(1.3, 'rotations', 55, 55)
+# br.driveMotors.move_tank(1, 'rotations', -55, -55)
 ResetGyro()
-debug("Turn Right")
-br.GyroTurn(90)
-debug("Move a small amount")
-br.driveMotors.move_tank(14, 'cm', 50, 50)
+br.GyroTurn(50)
 ResetGyro()
-debug("Turn Back")
-br.GyroTurn(-83)
+# br.AccelGyroDriveForward(40)
+# ResetGyro()
+# attachment.move_tank(.9, 'rotations' ,25, 25)
+# br.GyroTurn(69)
+# ResetGyro()
+# ResetGyro()
+br.driveMotors.move_tank(2, 'rotations', 50, 100)
+br.GyroTurn(95)
 ResetGyro()
-debug("Rotate Attachment")
-attachment.move_tank(.9, 'rotations' ,-25, -25)
-debug("Pick up Next Unit")
-br.AccelGyroDriveForward(20)
-ResetGyro()
-br.GyroTurn(59)
-br.driveMotors.move_tank(1.3, 'rotations', 55, 55)
-ResetGyro()
-br.GyroTurn(65)
-ResetGyro()
-br.AccelGyroDriveForward(40)
-ResetGyro()
-attachment.move_tank(.9, 'rotations' ,25, 25)
-br.GyroTurn(69)
-ResetGyro()
-br.driveMotors.move_tank(6, 'rotations', 100, 90)
+# ResetGyro()
+# br.AccelGyroDriveForward(40)
+# ResetGyro()
+# br.GyroTurn(90)
+# br.driveMotors.move_tank(8, 'rotations', 100, 100)
+br.AccelGyroDriveForward(50)
+sys.exit("1")
