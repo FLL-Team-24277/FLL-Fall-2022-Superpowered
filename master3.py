e8b2835f633d11090d5322ca65a29b636ab6b4ee
slyced <jonaselsass@yahoo.com>
2022-12-07 16:14:39 -0600

    Cleveland's Mission

--- master3.py
+++ master3.py
@@ -111,14 +111,15 @@ def mission4():
 def mission5():
 
     # First put the truck in the ellipse
-    # New COmment from zack
+    br.hub.motion_sensor.reset_yaw_angle()
     br.WaitForSeconds(.5)
     br.MoveTank(78, "cm", 100, 100)
     br.MoveTank(-78, "cm", 100, 100)
+    
     # tv mission
     br.WaitForButtonPress()
     br.MoveTank(46,"cm",50,50)
-    br.MoveTank(-36,"cm",50,50)
+    br.MoveTank(-40,"cm",50,50)
     # After returning to launch area, place the robot in the
     # launch spot by hand, and then press the left button
     # to continue
@@ -156,7 +157,7 @@ def mission5():
     
 
     # operate the windmill three times
-    for i in range(3):
+    for i in range(4):
         br.MoveTank(amount=t, unit= "seconds", \
             left_speed=rammingspeed, right_speed=rammingspeed)
         br.WaitForSeconds(delay)
