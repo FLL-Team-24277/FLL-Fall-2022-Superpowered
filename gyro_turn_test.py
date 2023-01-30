18719399ccebccc8a2dc9c0f6e9a54ac9d656e87
Skip Morrow <skip@pelorus.org>
2023-01-30 17:51:14 -0500

    Moved logging to list withh write at the end

--- gyro_turn_test.py
+++ gyro_turn_test.py
@@ -4,8 +4,9 @@ import sys
 import utime
 from spike import (MotionSensor, MotorPair)
 from spike.control import wait_for_seconds
+import gc
 
-f = open("log2.txt", "a")
+logList = []
 debugMode = True
 _leftDriveMotorPort = 'E'
 _rightDriveMotorPort = 'A'
@@ -15,10 +16,21 @@ driveMotors = MotorPair(_leftDriveMotorPort,
 def Log(topic, msg):
     # Example usage
     # Log("GyroTurn", "Starting a gyro turn")
-        #t = '%4d-%02d-%02d %02d:%02d:%02d' % utime.localtime() [:6]
-        t = utime.ticks_ms()
-        f.write(str(t) + " [" + topic + "] " + msg)
-    #f.close()
+    #t = '%4d-%02d-%02d %02d:%02d:%02d' % utime.localtime() [:6]
+    t = str(utime.ticks_ms())
+    entry = t + " [" + topic + "] " + msg
+    logList.append(entry)
+    if (len(logList) % 25 == 0):
+        gc.collect()
+    # print(entry)
+
+def WriteLog():
+    with open("log2.txt", "a") as f:
+        for item in logList:
+            # write each item on a new line
+            # print("writing " + item)
+            f.write("%s\n" % item)
+    logList.clear()
 
 def GyroTurn(angle):
     """
@@ -44,7 +56,7 @@ def GyroTurn(angle):
 
     # open output file for logging
     if debugMode:
-        Log("Gyroturn", "Turning to heading " + str(angle) + "\n")
+        Log("Gyroturn", "Turning to heading " + str(angle))
 
     # Reset Yaw Angle
     MotionSensor().reset_yaw_angle()
@@ -56,36 +68,57 @@ def GyroTurn(angle):
     gyroTurnSpeed = 10
     # Tests if the angle is positive.
     yawAngle = MotionSensor().get_yaw_angle()
+    max_loops = 200
+    loops = 0
     if (angle > 0):
         driveMotors.start_tank(gyroTurnSpeed, -gyroTurnSpeed)
-        while (yawAngle < angle):
+        while (yawAngle < angle and loops < max_loops):
             # If it it is positive it starts turning right.
             yawAngle = MotionSensor().get_yaw_angle()
             if (debugMode == True):
-                Log("Gyroturn", "Current heading " + str(yawAngle) + "\n")
+                Log("Gyroturn", "(" + str(loops) + ") Current heading " + str(yawAngle))
+            loops += 1
+            print(str(loops))
     else:
         driveMotors.start_tank(-gyroTurnSpeed, gyroTurnSpeed)
-        while (yawAngle > angle):
+        while (yawAngle > angle and loops < max_loops):
             # If it it is positive it starts turning right.
             yawAngle = MotionSensor().get_yaw_angle()
             if (debugMode == True):
-                Log("Gyroturn", "Current heading " + str(yawAngle) + "\n")
+                Log("Gyroturn", "(" + str(loops) + ") Current heading " + str(yawAngle))
+            loops += 1
+            print(str(loops))
 
+    # did we max out on loops?
+    if (loops == max_loops):
+        Log("Gyroturn", "Aborted; reached max_loops")
     # Stops when it is it has reached the desired angle
     if (debugMode == True):
-        Log("Gyroturn", "Calling stop(). Current heading " + str(MotionSensor().get_yaw_angle()) + "\n")
+        Log("Gyroturn", "Calling stop(). Current heading " + str(MotionSensor().get_yaw_angle()))
     driveMotors.stop()
     if (debugMode == True):
-        Log("Gyroturn", "stop() has been called. Current heading " + str(MotionSensor().get_yaw_angle()) + "\n")
+        Log("Gyroturn", "stop() has been called. Current heading " + str(MotionSensor().get_yaw_angle()))
     wait_for_seconds(0.5)
     if (debugMode == True):
-        Log("Gyroturn", "Final heading " + str(MotionSensor().get_yaw_angle()) + "\n")
+        Log("Gyroturn", "Final heading " + str(MotionSensor().get_yaw_angle()))
 
+    WriteLog()
 
 # Now, the test program
+# test = []
+# for i in range(0, 800):
+#     t = str(utime.ticks_ms())
+#     # entry = t
+#     entry = "%d  [Gyroturn] Current heading 35" % utime.ticks_ms()
+#     test.append(entry)
+#     if (i % 50 == 0) :
+#         gc.collect()
+    
 
-GyroTurn(45)
-wait_for_seconds(1)
+# for item in test:
+#     # write each item on a new line
+#     print(item)
+
 GyroTurn(-45)
-
-f.close()
+wait_for_seconds(1)
+GyroTurn(45)
